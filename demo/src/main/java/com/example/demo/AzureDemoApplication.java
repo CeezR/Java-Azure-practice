56d04d2ad3d32e5c3ec6cd3227a38bdf586c81c6
Cesar Forthomme <cesar.forthomme@appliedtechnology.se>
2023-07-10 15:27:28 +0200

    test: new

--- demo/src/main/java/com/example/demo/AzureDemoApplication.java
+++ demo/src/main/java/com/example/demo/AzureDemoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AzureDemoApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(AzureDemoApplication.class, args);
 	}
